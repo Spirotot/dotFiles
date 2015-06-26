f4e8d2aeba2c7cb23240b0613aeffc501149d110
Aaron Fields <aaron.fields@jhuapl.edu>
2015-06-26 16:44:13 -0400

    Updated timer with workouts.

--- bin/timer.sh
+++ bin/timer.sh
@@ -1,23 +1,53 @@
 #!/bin/bash
+workouts[0]="Scissor Twist (https://www.youtube.com/watch?v=Tg0O8KvvjtE )"
+workouts[1]="Row Your Boat (https://www.youtube.com/watch?v=q3F2Bao2Xnk)"
+workouts[2]="Banana X (http://www.buildahardbody.com/p90x2-ab-ripper/ )"
+workouts[3]="Oblique roll (http://www.buildahardbody.com/p90x2-ab-ripper/ )"
+workouts[4]="Roll V Hold (https://www.youtube.com/watch?v=ZyLTL4oSJgU )"
+workouts[5]="Abrinome (http://www.buildahardbody.com/p90x2-ab-ripper/ )"
+workouts[6]="Gate Bridge Lifts (https://www.youtube.com/watch?v=GE_9BlowWBc )"
+workouts[7]="Phelan Twist (http://www.buildahardbody.com/p90x2-ab-ripper/ )"
+workouts[8]="Scissor Clapper (http://www.buildahardbody.com/p90x2-ab-ripper/ )"
+workouts[9]="Tornado (http://www.buildahardbody.com/p90x2-ab-ripper/ )"
+workouts[10]="Balance Bikes (http://www.buildahardbody.com/p90x2-ab-ripper/ )"
+
 limit=$1
 echo
 echo "Timing $limit minutes..."
 echo
-counter=0
-while [ $counter != $limit ]; do
-   echo "$counter minutes so far...";
-   sleep 60
-   let "counter = $counter + 1"
-done
-if [ $counter = $limit ]; then
-   echo
-   echo "Time's up - $counter minutes have elapsed!"
 
-   counter=0
-   while [ $counter != 10 ]; do
-   	mpg123 ~/beep.mp3
-	let "counter = $counter + 1"
-   done
-
-   exit 0
+index=0
+if [ -f ~/bin/last_workout ]; then
+    let "index = `cat ~/bin/last_workout` + 1"
+    if [ $index = 11 ]; then
+       index=0
+    fi
 fi
+
+while [ 1 = 1 ]; do
+    read -p "Do ${workouts[$index]}, and press Enter when done."
+
+    echo $index > ~/bin/last_workout
+
+    counter=0
+    while [ $counter != $limit ]; do
+       echo "$counter minutes so far...";
+       sleep 60
+       let "counter = $counter + 1"
+    done
+    if [ $counter = $limit ]; then
+       echo
+       echo "Time's up - $counter minutes have elapsed!"
+
+       counter=0
+       while [ $counter != 10 ]; do
+        mpg123 ~/beep.mp3 > /dev/null 2>&1
+        let "counter = $counter + 1"
+       done
+   fi
+
+   let "index = $index + 1"
+   if [ $index = 11 ]; then
+       index=0
+   fi
+done
