918a15a8b62ae11b9b429bf713c75e69692e6ad7
Aaron Fields <Aaron.Fields@jhuapl.edu>
2016-01-03 16:21:37 -0500

    Arch stuff.

--- bin/suspendandlock.sh
+++ bin/suspendandlock.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-i3lock -d && dbus-send --system --print-reply \
-    --dest="org.freedesktop.UPower" \
-    /org/freedesktop/UPower \
-    org.freedesktop.UPower.Suspend
+#i3lock -d && dbus-send --system --print-reply \
+#    --dest="org.freedesktop.UPower" \
+#    /org/freedesktop/UPower \
+#    org.freedesktop.UPower.Suspend
+
+i3lock -d && systemctl suspend
