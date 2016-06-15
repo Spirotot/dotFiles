4e4709bd7c50ec72bd6a1cc762a520c31ecb89ea
Aaron Fields <spirotot@gmail.com>
2016-06-14 21:48:02 -0400

    Suspending fixes.

--- bin/suspendandlock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#i3lock -d && dbus-send --system --print-reply \
-#    --dest="org.freedesktop.UPower" \
-#    /org/freedesktop/UPower \
-#    org.freedesktop.UPower.Suspend
-
-~/bin/lock.sh &
-sleep 3 && systemctl suspend
