ac618627df5ce96f317602adaf45082747341ee0
Aaron Fields <spirotot@gmail.com>
2016-10-29 12:59:29 -0400

    Prompt for username when installing service files.

--- .config/service_files/create_syminks.sh
+++ .config/service_files/create_syminks.sh
@@ -3,6 +3,11 @@ echo "Run as root/sudo"
 curdir=`pwd`
 user=$1
 
+if [ X$user = X ]; then
+    echo "You need to specify a user."
+    exit
+fi
+
 mv ./suspendautolock.service ./suspendautolock@$user.service
 for file in `find . -name "*.service" -printf '%P\n'`; do
     echo "curdir: $curdir"
