18e0e15a974067322b7843e3e7bf68c3d5250380
Aaron Fields <spirotot@gmail.com>
2017-11-27 16:56:48 -0500

    Ubuntu tweaks.

--- .zshrc
+++ .zshrc
@@ -5,7 +5,8 @@ then
 fi
 export ZSH_TMUX_AUTOSTART_ONCE=false
 export ZSH_TMUX_AUTOCONNECT=false
-source ~/antigen/antigen.zsh
+
+source /usr/share/zsh-antigen/antigen.zsh
 
 #http://www.lowlevelmanager.com/2012/04/zsh-history-extend-and-persist.html
 setopt APPEND_HISTORY
@@ -75,10 +76,12 @@ antigen bundle python
 antigen bundle systemd
 antigen bundle gnu-utils
 antigen bundle joel-porquet/zsh-dircolors-solarized # This uses `dircolors` to populate LS_COLORS with the proper solarized colors...
-antigen bundle marzocchi/zsh-notify
-zstyle ':notify:*' command-complete-timeout 15
-zstyle ':notify:*' error-title ERROR
-zstyle ':notify:*' success-title SUCCESS
+#antigen bundle marzocchi/zsh-notify
+eval "$(ntfy shell-integration)"
+
+#zstyle ':notify:*' command-complete-timeout 15
+#zstyle ':notify:*' error-title ERROR
+#zstyle ':notify:*' success-title SUCCESS
 #source ~/.antigen/bundles/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh.git/lib/nvm.zsh
 source ~/.antigen/bundles/robbyrussell/oh-my-zsh/lib/nvm.zsh
 autoload -U colors && colors # https://stackoverflow.com/questions/26829821/zsh-theme-not-working-properly-on-osx
--- install.sh
+++ install.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-sudo eopkg it tmux gdb make cmake gcc g++ glibc-devel linux-headers python-devel golang rust cargo pip zsh diffutils llvm-clang atool docker taskwarrior pkg-config dbus-devel dbus-glib-devel strace vagrant
+#sudo eopkg it tmux gdb make cmake gcc g++ glibc-devel linux-headers python-devel golang rust cargo pip zsh diffutils llvm-clang atool docker taskwarrior pkg-config dbus-devel dbus-glib-devel strace vagrant
+
+sudo apt-get -y remove --purge thunderbird rhythmbox
+
+sudo apt-get update
+sudo apt-get -y install curl
+curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+sudo apt-add-repository -y ppa:tista/adapta
+sudo add-apt-repository -y ppa:papirus/papirus
+
+sudo apt-get update
+
+sudo apt-get -y install vim tmux gdb build-essential linux-headers-$(uname --kernel-release) python-dev golang rustc cargo python-pip zsh diffutils tig meld clang  atool pandoc texlive bibtool docker.io taskwarrior timewarrior bugwarrior tasksh evolution strace vagrant gnome-tweak-tool breeze-cursor-theme papirus-icon-theme adapta-gtk-theme chromium-browser virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso fonts-roboto libinput-tools xdotool wmctrl signal-desktop tlp powertop
 
 # Make Docker happy
 sudo usermod -a -G docker $USER
+sudo usermod -a -G input $USER
 
 # Install/configure VIM & plugins...
 mkdir -p ~/.vim/bundle
@@ -22,30 +36,38 @@ git clone https://github.com/zsh-users/antigen.git ~/antigen
 sudo pip install tasklib
 chsh -s $(which zsh)
 
+# NTFY: https://github.com/dschep/ntfy
+sudo pip install ntfy
+
 # Make sure tmux is happy.
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Install timewarrior
-sudo snap install timew-spirotot
-sudo snap alias timew-spirotot.timew timew
+#sudo snap install timew-spirotot
+#sudo snap alias timew-spirotot.timew timew
 
 # Set up taskwarrior
-mkdir -p ~/.task/hooks
+#mkdir -p ~/.task/hooks
 #ln -s /snap/timew-spirotot/current/share/doc/timew/ext/on-modify.timewarrior ~/.task/hooks/on-modify.timewarrior
-cp /snap/timew-spirotot/current/share/doc/timew/ext/on-modify.timewarrior ~/.task/hooks/
-chmod +x ~/.task/hooks/on-modify.timewarrior
+#cp /snap/timew-spirotot/current/share/doc/timew/ext/on-modify.timewarrior ~/.task/hooks/
+#chmod +x ~/.task/hooks/on-modify.timewarrior
 
 
 # And Bugwarrior..
-sudo pip install bugwarrior
-sudo pip install "bugwarrior[keyring]"
+#sudo pip install bugwarrior
+#sudo pip install "bugwarrior[keyring]"
 cd ~/.config/systemd/user && systemctl --user enable bugwarrior-pull.timer && systemctl --user start bugwarrior-pull.timer && cd -
 
 # And tasksh.
-cd ~/bin && wget https://raw.githubusercontent.com/chihchun/snapcraft-docker/a8d3161a61cb921e1f895742209e66f73c6579e6/snapcraft-docker && chmod +x snapcraft-docker && cd -
-cd /tmp
-git clone https://github.com/Spirotot/tasksh-spirotot.git && cd tasksh-spirotot && snapcraft-docker ./build.sh && sudo snap install --dangerous --classic ./*.snap && cd ~/
+#cd ~/bin && wget https://raw.githubusercontent.com/chihchun/snapcraft-docker/a8d3161a61cb921e1f895742209e66f73c6579e6/snapcraft-docker && chmod +x snapcraft-docker && cd -
+#cd /tmp
+#git clone https://github.com/Spirotot/tasksh-spirotot.git && cd tasksh-spirotot && snapcraft-docker ./build.sh && sudo snap install --dangerous --classic ./*.snap && cd ~/
 
-sudo snap alias tasksh-spirotot.tasksh tasksh
-sudo snap alias timew-spirotot.timew timew
+#sudo snap alias tasksh-spirotot.tasksh tasksh
+#sudo snap alias timew-spirotot.timew timew
+
+# Libinput
+git clone http://github.com/bulletmark/libinput-gestures
+cd libinput-gestures
+sudo ./libinput-gestures-setup install
